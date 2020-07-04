7b98d02ad4f5790fae193a902766ffe2012a8395
GargSamarth <64011957+GargSamarth@users.noreply.github.com>
2020-07-04 05:35:53 +0530

    Add files via upload
    New_web

---
+++ JS/effects.js
@@ -0,0 +1,55 @@
+// main header
+$(document).ready(function(){
+  $(".fade-in-1").fadeIn(2000,function(){
+  });
+});
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if(document.body.scrollTop > 1 || document.documentElement.scrollTop > 1){
+    $(".slide-down-effect1").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 700 || document.documentElement.scrollTop > 700){
+    $(".slide-down-effect2").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300){
+    $(".slide-down-effect3").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100){
+    $(".slide-down-effect4").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 2800 || document.documentElement.scrollTop > 2800){
+    $(".slide-down-effect5").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 3300 || document.documentElement.scrollTop > 3300){
+    $(".slide-down-effect6").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 4000 || document.documentElement.scrollTop > 4000){
+    $(".slide-down-effect7").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 4600 || document.documentElement.scrollTop > 4600){
+    $(".slide-down-effect8").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 5300 || document.documentElement.scrollTop > 5300){
+    $(".slide-down-effect9").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 6000 || document.documentElement.scrollTop > 6000){
+    $(".slide-down-effect10").slideDown(600,function(){
+    });
+  }
+  if(document.body.scrollTop > 6700 || document.documentElement.scrollTop > 6700){
+    $(".slide-down-effect11").slideDown(1600,function(){
+    });
+  }
+};
